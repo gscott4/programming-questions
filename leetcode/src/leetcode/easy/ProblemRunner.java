d0400b9d40505357f860210c8f529f14437c8526
gscott4 <grayson.scott@outlook.com>
2019-03-11 11:15:07 -0400

    Valid Anagram

---
+++ leetcode/src/leetcode/easy/ProblemRunner.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+public class ProblemRunner {
+
+	public static void main(String[] args) {
+		
+		//ReverseString rs = new ReverseString();
+		//rs.reverseInit();
+		
+		ValidAnagram va = new ValidAnagram();
+		va.init();
+
+	}
+
+}
---
+++ leetcode/src/leetcode/easy/ValidAnagram.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+public class ValidAnagram {
+	
+	public void init() {
+		String word = "anagram";
+		String anagram = "nagaram";
+		
+		boolean isAnagram = isAnagram(word, anagram);
+		
+		if(isAnagram) {
+			System.out.println("Word is an anagram");
+		}
+	}
+
+	private boolean isAnagram(String s, String t) {
+		
+		if(s.length() != t.length()) {
+			return false;
+		}
+		int[] counts = new int[26];
+		for(int i = 0; i < s.length(); i++) {
+			counts[s.charAt(i) - 'a']++;
+			counts[t.charAt(i) - 'a']--;
+		}
+		
+		for(int i : counts) {
+			if(i != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
