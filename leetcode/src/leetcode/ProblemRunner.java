e86d175be3b2811f2074f321fcc85080e73cc5b1
gscott4 <grayson.scott@outlook.com>
2019-03-11 10:49:37 -0400

    Reverse a string

---
+++ leetcode/src/leetcode/ProblemRunner.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class ProblemRunner {
+
+	public static void main(String[] args) {
+		
+		ReverseString rs = new ReverseString();
+		rs.reverseInit();
+
+	}
+
+}
---
+++ leetcode/src/leetcode/ReverseString.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class ReverseString {
+
+	public void reverseInit() {
+		String word = "hello";
+		String reverse = reverseString(word);
+		System.out.println(reverse);
+	}
+	
+	public String reverseString(String s) {
+		char[] characters = s.toCharArray();
+		int i = 0;
+		int j = s.length() - 1;
+		while(i < j) {
+			char temp = characters[i];
+			characters[i++] = characters[j];
+			characters[j--] = temp;
+		}
+		
+		return new String(characters);
+	}
+	
+	
+}
